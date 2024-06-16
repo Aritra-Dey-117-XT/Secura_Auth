761faf5142cc18b65466d60c8176ee7663805e79
Aritra-Dey-117-XT <ece22127@iiitkalyani.ac.in>
2024-06-16 18:10:51 +0530

    Fixed a typo in logout route, the try block return  response had 'succes' instead of 'success'.

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -5,7 +5,7 @@ export async function GET() {
 
         const response = NextResponse.json({
             messsage: "User Logged Out Successfully",
-            succes: true
+            success: true
         }, {status: 200})
 
         response.cookies.set("token", "", {httpOnly: true, expires: new Date(0)})
